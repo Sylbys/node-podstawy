a89d92030c1fa5cf0e9624327291b15fe11462e8
Sylwia <tatu139@wp.pl>
2020-03-07 20:20:24 +0100

    wyrazenie funkcyjne

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-//obiekt global - cos jak window w przegladarce, udostepnia funkcje do uzycia w node
-let licznik=0;
-console.log('witaj ')
-setTimeout( () => {
-	console.log( 'minęła 1 sek, teraz co dwie sekundy wyswietle licznik') ;
-	const timer = setInterval( () => {
-		console.log(`minęło ${licznik+=2} sekund`);
-		if (licznik>10){
-			clearInterval(timer);
-		}
-	},2000);
-}, 1000);
-console.log('Obecnie znajdujesz sie w folderze'+__dirname);
-console.log('plik '+__filename);
+//funkcje tworzenie
 
-// const http = require('http');
 
-// const server = http.cerateServer((req,res) => {
-// 	const ip = res.socke
-// }).listen(3000);
+//normalna deklaracja funkcji - normal statement
+function mojaFunkcja(){
+	console.log('to jest mojaFunkcja');
+};
+
+
+//wyrazenie funkcyjne - function expression
+var przywitaj = function(){
+	console.log('Witaj ziom!');
+};
+
+//call function
+
+function powiadom(func){
+	func();
+	console.log('co tam robisz?');
+}
+
+powiadom(mojaFunkcja);
+
+powiadom(przywitaj);
