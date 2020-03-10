808b1fb7613e2139b59cf524176b2cd3e2c74cb5
Sylwia <tatu139@wp.pl>
2020-03-10 13:15:56 +0100

    costume events - util.inherits czyli dziedziczenie za pomoca modulu util

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const user = require('./user');
-const dzialania = require('./wieleFunkcji');
-const prosciej = require('./prosciej.js')
-const obiekt = require('./obiekt.js');
+//Event emmiter, costume events
+const events = require('events');
 
-//user.js pojedyncza funkcja
-user('Tomek');
-//wieleFunkcji.js jako wlasciwosci obiektu osobno
-dzialania.dodaj(3,4);
-dzialania.odejmij(3,4);
-dzialania.pomnoz(3,4);
-dzialania.podziel(3,4);
-//prosciej.js jako bezposrednie oksporty osobno
-prosciej.dodaj(15,7);
-prosciej.odejmij(19,8);
-//obiekt.js modul jako caly pojedynczy obiekt 
-obiekt.dodaj(255,2);
+const mojEmiter = new events.EventEmitter();
+
+mojEmiter.on('zdarzenie', (data)=> console.log (data));
+
+//musi byc po on bo nie zadziala (jakby symulacja zajscia zdarzenia, np jakby symulacja klikniecia)
+mojEmiter.emit('zdarzenie', {
+	wiadomosc : 'zaszlo zdarzenie',
+});//wyemituj zdarzenie o nazwie zdarzenie i zwroc dane jako obiekt
+
+
+//dziedziczenie eventu dzieki modulowi util
+const util = require('util');
+
+//tworze konstruktor klasy Persona ktora odziedziczy zdolnosc eventEmitter
+const Persona = function(name){
+	this.name = name;
+}
+
+//polecenia dodania dziedziczenia do klasy Persona
+util.inherits(Persona, events.EventEmitter);//(jaki element ma dziedziczyc cos, co ma dziedziczyc)
+
+const janek = new Persona('Janek');
+const zosia = new Persona('Zosia');
+const kasia = new Persona('Kasia');
+
+//dodamy tablice zawierajaca te elementy aby w petli nadac im zdarzenie na jakie maja reagowac
+const persony = [janek, zosia, kasia];
+
+persony.forEach( function(person){
+	person.on('powiedz', function (argument){ //dodanie even listenera do obiektow persony
+		console.log(`${person.name} powiedzial: ${argument}`);
+	});
+});
+
+//teraz mozemy na tych elementach wywolywac zdarzenia
+zosia.emit('powiedz','co sie gapisz?');
+janek.emit('powiedz', 'nic ci do tego');
